87704281f604014eb056f315ed28d38b8b697363
ademnr <ademnr2@gmail.com>
2025-07-19 23:55:08 +0100

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 // cors : origin: process.env.CLIENT_URL || 'http://localhost:3000'
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 // Rate limiting for API protection
